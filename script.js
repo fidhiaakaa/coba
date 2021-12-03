622eb6a5c06d3e94ad5a6edb98b9555fe64591f8
Fidhia Ainun Khofifah <90237196+fidhiaakaa@users.noreply.github.com>
2021-12-03 11:33:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,34 +4,6 @@ import { GLTFLoader } from './js/GLTFLoader.js';
 import * as dat from './jsm/libs/dat.gui.module.js'
 import { DragControls } from './js/DragControls.js';
 
-class FogGUIHelper {
-    constructor(fog, backgroundColor) {
-        this.fog = fog;
-        this.backgroundColor = backgroundColor;
-    }
-    get near() {
-        return this.fog.near;
-    }
-    set near(v) {
-        this.fog.near = v;
-        this.fog.far = Math.max(this.fog.far, v);
-    }
-    get far() {
-        return this.fog.far;
-    }
-    set far(v) {
-        this.fog.far = v;
-        this.fog.near = Math.min(this.fog.near, v);
-    }
-    get color() {
-        return `#${this.fog.color.getHexString()}`;
-    }
-    set color(hexString) {
-        this.fog.color.set(hexString);
-        this.backgroundColor.set(hexString);
-    }
-}
-
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
